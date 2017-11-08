c3528e7a9f018a97252b896b015f16964c6b3620
Dan Galbenu <dan.galbenu@yahoo.com>
2017-11-08 21:16:38 +0200

    plus 2 linii syso

--- Project18/src/Dan.java
+++ Project18/src/Dan.java
@@ -3,8 +3,9 @@ public class Dan {
 
 	public static void main(String[] args) {
 		
-		
+		System.out.println("o linie de syso");
 		System.out.println("pam pam");
+		System.out.println("inca o linie de syso");
 		
 	}
 
