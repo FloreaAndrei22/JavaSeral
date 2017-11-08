8f95f166a67ac7849ce09c307bab53a455ef80b0
Florin <florin@jademy.ro>
2017-11-08 19:30:16 +0200

    Am reparat o clasa si am mai creat o alta clasa
    - am adaugat un syso
    - am sters un mesaj prost
    - si altele......

--- Project18/src/Test.java
+++ Project18/src/Test.java
@@ -3,8 +3,9 @@ public class Test {
 	
 	public static void main(String[] args) {
 		System.out.println("TEST");
-		System.out.println("Again");
 		System.out.println("OKEYs");
+		
+		System.out.println("am mai lucrat 8 ore....");
 	}
 
 }
